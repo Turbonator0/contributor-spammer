892d72656522154dbd89e9002a5a1c015b96fb7d
Turbonator0 <78885232+Turbonator0@users.noreply.github.com>
2024-03-03 17:55:28 +0200

    Fix?

--- spammer.py
+++ spammer.py
@@ -3,8 +3,9 @@ import os
 import sys
 
 for i in range(int(sys.argv[1])):
-  with open("secret", "r+") as file:
+  with open("secret", "r") as file:
     content = file.read()
-    _hash = hashlib.sha256(content.encode("UTF-8")).hexdigest()
-    file.write(_hash)
-    os.system(f"git add secret && git commit -m {_hash}")
+    with open("secret", "w") as wfile:
+      _hash = hashlib.sha1(content.encode("UTF-8")).hexdigest()
+      wfile.write(_hash)
+      os.system(f"git add secret && git commit -m {_hash}")
