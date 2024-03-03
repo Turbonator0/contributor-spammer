bb5fbecb9cd57d06161dcf401b4e489bad917c2e
Turbonator0 <78885232+Turbonator0@users.noreply.github.com>
2024-03-03 18:13:12 +0200

    Stop opening a file for no reason

--- spammer.py
+++ spammer.py
@@ -1,13 +1,12 @@
 import hashlib
 import os
 import sys
+import random
 
 for i in range(int(sys.argv[1])):
-  with open("secret", "r") as file:
-    content = file.read()
-    with open("secret", "w") as wfile:
-      _hash = hashlib.sha1(content.encode("UTF-8")).hexdigest()
-      wfile.write(_hash)
-    os.system(f"git add secret && git commit -m {_hash}")
+  with open("secret", "w") as wfile:
+    _hash = hashlib.sha1(str(random.randint(1,1000)).encode("UTF-8")).hexdigest()
+    wfile.write(_hash)
+  os.system(f"git add secret && git commit -m {_hash}")
 # Automagically push to Github
 os.system("git push origin master")
